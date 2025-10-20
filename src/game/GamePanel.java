2517d45dcce6d642fdfd204deef7bc7bafc4e701
Kaila <Kaila@Familiaprizulu>
2025-10-19 21:48:48 -0500

    Ya se puede mover el cuadradito blanco :D (Implementé nuevo método de actualización de FPS)

--- src/game/GamePanel.java
+++ src/game/GamePanel.java
@@ -44,11 +44,10 @@ public class GamePanel extends JPanel implements Runnable{
     }
     
     @Override
-    public void run(){
+/*  public void run(){
+       
         while(gameThread != null){
-            
-//          System.out.println("El juego se esta ejecutando...");
-            
+                        
             double drawInterval = 1000000000/FPS; // 0,016666 seconds
             double nextDrawTime = System.nanoTime() + drawInterval;
             
@@ -77,6 +76,40 @@ public class GamePanel extends JPanel implements Runnable{
             }
             
         }
+    }*/
+    
+    public void run() {
+
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while (gameThread != null) {
+
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+            
+            if(timer >= 1000000000){
+                System.out.println("FPS:" + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+
+        }
+
     }
     
     public void update(){
