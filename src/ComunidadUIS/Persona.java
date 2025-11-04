28b595b73ae086e229b7801e58c4f1b54d952875
CENTIC <CENTIC@10.6.65.101>
2025-11-04 10:39:10 -0500

    Setters, getters y constructores de la clase Persona

--- src/ComunidadUIS/Persona.java
+++ src/ComunidadUIS/Persona.java
@@ -25,7 +25,77 @@ public abstract class Persona {
         this.setLugarOrigen(lugarOrigen);
         this.setColorPiel(colorPiel);
         this.setGenero(genero);
+        this.setOrientacionSexual(orientacionSexual);
+        this.setSexo(sexo);
+        this.setComunidadUIS(comunidadUIS);
+        this.setEstratoSocial(estratoSocial);
+        this.setSisben(sisben);
     }
+    
+    protected final void setName(String name){
+        this.name = name;
+    }
+    protected final void setEdad(int edad){
+        this.edad = edad;
+    }
+    protected final void setLugarOrigen(String lugarOrigen){
+        this.lugarOrigen = lugarOrigen;
+    }
+    protected final void setColorPiel(String colorPiel){
+        this.colorPiel = colorPiel;
+    }
+    protected final void setGenero(String genero){
+        this.genero = genero;
+    }
+    protected final void setOrientacionSexual(String orientacionSexual){
+        this.orientacionSexual = orientacionSexual;
+    }
+    protected final void setSexo(String sexo){
+        this.sexo = sexo;
+    }
+    protected final void setComunidadUIS(boolean comunidadUIS){
+        this.comunidadUIS = comunidadUIS;
+    }
+    protected final void setEstratoSocial(int estratoSocial){
+        this.estratoSocial = estratoSocial;
+    }
+    protected final void setSisben(String sisben){
+        this.sisben = sisben;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    public int getEdad(){
+        return this.edad;
+    }
+    public String getLugarOrigen(){
+        return this.lugarOrigen;
+    }
+    public String getColorPiel(){
+        return this.colorPiel;
+    }
+    public String getGenero(){
+        return this.genero;
+    }
+    public String getOrientacionSexual(){
+        return this.orientacionSexual;
+    }
+    public String getSexo(){
+        return this.sexo;
+    }
+    public boolean getComunidadUIS(){
+        return this.comunidadUIS;
+    }
+    public int getEstratoSocial(){
+        return this.estratoSocial;
+    }
+    public String getSisben(){
+        return this.sisben;
+    }
+            
+    
+    
             
     
     
